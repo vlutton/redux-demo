c35ae2850545c19ad2036db712ddc28f8de510d9
Vincent Lutton <vlutton@Vincents-MacBook-Pro.local>
2016-01-24 19:13:21 -0600

    first commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,8 @@
+
+/// <reference path="react/react.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="es6-promises/es6-promises.d.ts" />
+/// <reference path="typescript/typescript.d.ts" />
+/// <reference path="assertion-error/assertion-error.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
